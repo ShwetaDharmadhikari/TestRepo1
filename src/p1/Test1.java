ec12a12e4c7e79b660f934e4e0107d3a766c3656
shweta dharmadhikari <shweta.dharmadhikari7@gmail.com>
2022-02-07 22:35:59 +0530

    Update

--- src/p1/Test1.java
+++ src/p1/Test1.java
@@ -2,11 +2,31 @@ package p1;
 
 public class Test1 {
 
+	void m1() {
+		try {
+			System.out.println(1);
+			int x = 10;
+			int y = 3;
+			try {
+				System.out.println("Hi");
+				int ans = x / (y - 3);
+				System.out.println(ans);
+			} catch (ArithmeticException ne) {
+				System.out.println("In inner catch");
+			}
+			System.out.println(2);
+		} catch (ArithmeticException a) {
+			System.out.println(3);
+		}
+		System.out.println(4);
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("In P1 Test1.....");
-		System.out.println("In P1 Test1");
-		System.out.println("Hello");
+		new Test1().m1();
+		System.out.println("end");
+		System.out.println("                  ");
+		System.out.println("endddd");
 	}
 
 }
