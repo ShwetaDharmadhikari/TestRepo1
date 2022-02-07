8af5a0170088a290e819cdf9abc0d733a6c7c5c3
shweta dharmadhikari <shweta.dharmadhikari7@gmail.com>
2022-02-07 22:38:18 +0530

    Update

--- src/p2/Test2.java
+++ src/p2/Test2.java
@@ -1,11 +1,35 @@
 package p2;
 
 public class Test2 {
-
+	
+	void m1(String str) {
+		try {
+			System.out.println(str);
+			str=str.substring(9);
+			System.out.println(str.charAt(6));
+			
+		}catch(StringIndexOutOfBoundsException ab) {
+			System.out.println("Hi");
+			try {
+				System.out.println(str.charAt(str.length()));
+			}catch(Exception e) {
+				System.out.println("In exception");
+			}
+			System.out.println(10);
+		}finally {
+			try {
+			int x=10/(str.length()-4);
+			System.out.println(x);
+			}catch(NullPointerException ne) {
+				System.out.println(15);
+			}
+			
+		}
+		System.out.println(4);
+		}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("In P2 Test2");
-		System.out.println("Line 7 P1 Test1 B2 Branch");
+		new Test2().m1("technocredits");
 	}
 
 }
